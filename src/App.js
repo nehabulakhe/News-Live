3d679cd039719ff5157337812e5d36b53f372cf9
Neha Bulakhe <nehabulakhe0922@gmail.com>
2023-08-25 12:22:37 +0530

    Build news item basic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 
 import './App.css';
 import React, { Component } from 'react'
+import Navbar from './Components/Navbar';
+import News from './Components/News';
+
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        Hello 
+      <>
+        <Navbar/>
+        <News>
+          
+        </News>
+        
+      </>
       </div>
     )
   }
---
+++ src/Components/News.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export class News extends Component {
+  render() {
+    return (
+      <div>
+        <div className="container">
+            <h2>Top Headlines</h2>
+        <div className="row">
+            <div className="col-md-4">
+            <NewsItem title="my title" description="my desc"/>
+            </div>
+    
+            <div className="col-md-4">
+            <NewsItem title="my title" description="my desc"/>
+            </div>
+        
+            <div className="col-md-4">
+            <NewsItem title="my title" description="my desc"/>
+            </div>
+        </div>
+      </div>
+      </div>
+    )
+  }
+}
+
+export default News
