ca7af6ace2ca73512bdbaddb1dabae93b5220d8f
Neha Bulakhe <nehabulakhe0922@gmail.com>
2023-08-30 12:33:22 +0530

    navbar title margin change

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ export default class Navbar extends Component {
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">News-Live</a>
+    <a className="navbar-brand" href="/" style={{marginLeft:'30px'}}>News-Live</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
